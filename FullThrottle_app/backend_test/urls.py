27fbdcd17ba96ca2f03f0406b90f3209e28f7f9e
Sailok Chinta <sailokchinta2012@gmail.com>
2020-08-13 15:07:33 +0530

    Update urls.py

--- FullThrottle_app/backend_test/urls.py
+++ FullThrottle_app/backend_test/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'app', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^get_user_activity/$', views.UserList.as_view()),
 ]
